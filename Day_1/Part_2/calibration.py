713aff5fc2f35091534480becac495eced2fbace
CDixon0416 <bluemonkeyboys@gmail.com>
2024-02-02 15:57:52 -0500

    Reverse strings != original string who knew?

--- Day_1/Part_2/calibration.py
+++ Day_1/Part_2/calibration.py
@@ -7,18 +7,64 @@
 from datetime import date
 import re
 
-
 # Parse a given string for the first digit found
 def ParseFirstDigit(input_line):
-    regex = re.search("[0-9]", input_line)
-    return input_line[regex.start()]
+    int_regex = re.search("[0-9]", input_line)
+    str_regex = re.findall("one|two|three|four|five|six|seven|eight|nine", input_line)
+
+    if not str_regex or int_regex.start() < re.search(str_regex[0], input_line).start():
+        return input_line[int_regex.start()]
+    else:
+        match str_regex[0]:
+            case "one":
+                return 1
+            case "two":
+                return 2
+            case "three":
+                return 3
+            case "four":
+                return 4
+            case "five":
+                return 5
+            case "six":
+                return 6
+            case "seven":
+                return 7
+            case "eight":
+                return 8
+            case "nine":
+                return 9
 
 # Parse a string in reverse for the first digit found.
 # This will give us the last digit on a given input.
 def ParseLastDigit(input_line):
     reverse_input=input_line[::-1]
-    regex=re.search("[0-9]",reverse_input)
-    return reverse_input[regex.start()]
+
+    int_regex = re.search("[1-9]", reverse_input)
+    str_regex = re.findall("eno|owt|eerht|ruof|evif|xis|neves|thgie|enin", reverse_input)
+
+    if not str_regex or int_regex.start() < re.search(str_regex[0], reverse_input).start():
+        return reverse_input[int_regex.start()]
+    else:
+        match str_regex[0]:
+            case "eno":
+                return 1
+            case "owt":
+                return 2
+            case "eerht":
+                return 3
+            case "ruof":
+                return 4
+            case "evif":
+                return 5
+            case "xis":
+                return 6
+            case "neves":
+                return 7
+            case "thgie":
+                return 8
+            case "enin":
+                return 9
 
 # Add our two values together to create our initial calibration.
 def CreateInitialCalibration(input_line):
